bfcbefac8b61903e8032f36da85428d37a0ade02
bobomurod <bobomurod.boboev@yandex.ru>
2019-05-29 06:13:49 +0500

    first commit

---
+++ index.js
@@ -0,0 +1,71 @@
+//bismillah
+
+// @coding Bobomurod Boboev  https://t.me/b0b0mur0d   https://github.com/bobomurod
+
+
+const express = require("express");
+const volleyball = require("volleyball");
+const bcrypt = require("bcryptjs");
+const cors = require("cors");
+
+
+//const bodyParser = require('body-parser');
+
+//const morgan = require('morgan');
+//const cors = require('cors');
+
+const app = express();
+
+const auth = require("./auth/index.js");
+
+const users = require("./users/index.js");
+
+app.use(volleyball);
+app.use(express.json());
+app.use(middlewares.checkTokenSetUser);
+
+app.use(
+  cors({
+    origin: ["http://localhost:8080", "http://route1.myapp.local:8081", "http://localhost:8082", "http://localhost:8091"]
+  })
+);
+// app.use(bodyParser.json())
+// app.use(bodyParser.urlencoded( { extended: false } ))
+
+//app.use(morgan('dev'));
+//app.use(cors());
+
+
+app.get("/", (req, res) => {
+  res.json({
+    message: "Hello World! ",
+    user: req.user
+  });
+});
+
+app.use("/auth", auth);
+
+app.use("/users", users);
+
+
+function notFound(req, res, next) {
+  res.status(404);
+  const error = new Error("Not Found - " + req.originalUrl);
+  next(error);
+}
+
+function errorHandler(err, req, res, next) {
+  res.status(res.statusCode || 500);
+  res.json({
+    message: err.message,
+    stack: err.stack
+  });
+}
+
+app.use(notFound);
+app.use(errorHandler);
+
+const port = process.env.PORT || 5000;
+app.listen(port, () => {
+  console.log("Listening on port", port);
+});
